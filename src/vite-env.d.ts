3ce7cdc25eddf9163be8443eb4b3060034839812
MinGu Jeong <mingu830@gmail.com>
2024-02-07 18:41:21 +0900

    chore: env 타입 정의

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="vite/client" />
+
+export type EnvVar = {
+  VITE_NODE_ENV: "development" | "production";
+  VITE_REACT_APP_API_PATH: string;
+}
+
+
+declare global {
+  interface ImportMetaEnv extends EnvVar {}
+}
+
