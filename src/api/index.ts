8cbd172919653ea84e18a32071e744fdeeb8ca19
chaeseungyun <thslr57@naver.com>
2024-02-07 17:48:46 +0900

    chore: accessClient 설정

---
+++ src/api/index.ts
@@ -0,0 +1,27 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-underscore-dangle */
+import axios from 'axios';
+import { API_PATH } from 'config/constants';
+
+const accessClient = axios.create({
+  baseURL: `${API_PATH}`,
+  timeout: 2000,
+});
+
+accessClient.interceptors.request.use(
+  (config) => {
+    const accessToken = sessionStorage.getItem('access_token');
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+      return config;
+    }
+    return config;
+  },
+);
+
+accessClient.interceptors.response.use(
+  (response) => response.data
+)
+
+
+export { accessClient };
