2f9c793e59445c439c96f41e434fe54d800f8515
MinGu Jeong <mingu830@gmail.com>
2024-02-09 01:51:23 +0900

    chore: react 전역참조 에러 off

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -11,6 +11,7 @@ module.exports = {
   plugins: ['react-refresh'],
   rules: {
     "no-unused-vars": "off",
-    "@typescript-eslint/no-unused-vars": "warn"
+    "@typescript-eslint/no-unused-vars": "warn",
+    "react/react-in-jsx-scope": "off",
   }
 }
