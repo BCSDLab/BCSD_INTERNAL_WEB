030d76a74f06921a4ea66d68bef3518c148f1a27
MinGu Jeong <mingu830@gmail.com>
2024-02-09 21:06:50 +0900

    refacotr: 색상 변수화

--- src/layout/DefaultLayout/style.ts
+++ src/layout/DefaultLayout/style.ts
@@ -1,19 +1,24 @@
 import {css} from '@emotion/react';
 
+const color = {
+  gray: '#F9FAFB',
+  borderGray: '#e0e0e0'
+};
+
 export const sideBar = css`
   display: flex;
   flex-direction: column;
   width: 250px;
   height: 100vh;
-  background-color: #F9FAFB;
-  border-right: 1px solid #e0e0e0;
+  background-color: ${color.gray};
+  border-right: 1px solid ${color.borderGray};
 `;
 
 export const topBar = css`
   width: 100%;
   height: 100px;
-  background-color: #F9FAFB;
-  border-bottom: 1px solid #e0e0e0;
+  background-color: ${color.gray};
+  border-bottom: 1px solid ${color.borderGray};
 `;
 
 export const content = css`
@@ -25,7 +30,7 @@ export const content = css`
 
 export const container = css`
   display: flex;
-  background-color: #F9FAFB;
+  background-color: ${color.gray};
   width: 100%;
   height: 100%;
 `;
