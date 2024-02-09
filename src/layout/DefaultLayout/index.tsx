464d95d77f33f9a3ba6acde0b84f805d1ec54cd8
MinGu Jeong <mingu830@gmail.com>
2024-02-09 20:57:38 +0900

    refactor: 중첩되는 style 접미사 제거

--- src/layout/DefaultLayout/index.tsx
+++ src/layout/DefaultLayout/index.tsx
@@ -3,13 +3,13 @@ import * as S from './style';
 
 export default function DefaultLayout() {
   return (
-    <div css={S.containerStyle}>
-      <div css={S.sideBarStyle}>
-        <img src="/src/assets/image/BCSD_logo-01.png" alt="logo" css={S.logoStyle} />
+    <div css={S.container}>
+      <div css={S.sideBar}>
+        <img src="/src/assets/image/BCSD_logo-01.png" alt="logo" css={S.logo} />
         <Button variant="outlined" color="secondary" sx={{ marginTop: '20px' }}>회원정보</Button>
       </div>
-      <div css={S.contentStyle}>
-        <div css={S.topBarStyle}></div>
+      <div css={S.content}>
+        <div css={S.topBar}></div>
       </div>
     </div>
   );
--- src/layout/DefaultLayout/style.ts
+++ src/layout/DefaultLayout/style.ts
@@ -1,6 +1,6 @@
 import {css} from '@emotion/react';
 
-export const sideBarStyle = css`
+export const sideBar = css`
   display: flex;
   flex-direction: column;
   width: 250px;
@@ -9,28 +9,28 @@ export const sideBarStyle = css`
   border-right: 1px solid #e0e0e0;
 `;
 
-export const topBarStyle = css`
+export const topBar = css`
   width: 100%;
   height: 100px;
   background-color: #F9FAFB;
   border-bottom: 1px solid #e0e0e0;
 `;
 
-export const contentStyle = css`
+export const content = css`
   display: flex;
   flex-direction: column;
   width: 100%;
   height: 100%;
 `;
 
-export const containerStyle = css`
+export const container = css`
   display: flex;
   background-color: #F9FAFB;
   width: 100%;
   height: 100%;
 `;
 
-export const logoStyle = css`
+export const logo = css`
   width: 100%;
   margin-top: -10px;
 `;
