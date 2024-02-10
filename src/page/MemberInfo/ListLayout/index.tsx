ca4d284b24072b9f65a3bdd21f371b0e5b6938fe
MinGu Jeong <mingu830@gmail.com>
2024-02-11 02:50:57 +0900

    feat: 리스트 스타일 속성 추가

--- src/page/MemberInfo/ListLayout/index.tsx
+++ src/page/MemberInfo/ListLayout/index.tsx
@@ -2,16 +2,16 @@ import * as S from './style';
 
 export default function ListLayout() {
   return(
-    <div css={S.attribute}>
-      <div>이름</div>
-      <div>트랙</div>
-      <div>직책</div>
-      <div>상태</div>
-      <div>소속</div>
-      <div>학부</div>
-      <div>학번</div>
-      <div>전화번호</div>
-      <div>이메일</div>
+    <div css={S.attributeContainer}>
+      <div css={S.attribute}>이름</div>
+      <div css={S.attribute}>트랙</div>
+      <div css={S.attribute}>직책</div>
+      <div css={S.attribute}>상태</div>
+      <div css={S.attribute}>소속</div>
+      <div css={S.attribute}>학부</div>
+      <div css={S.longAttribute}>학번</div>
+      <div css={S.longAttribute}>전화번호</div>
+      <div css={S.longAttribute}>이메일</div>
       </div>
   )
 }
--- src/page/MemberInfo/ListLayout/style.ts
+++ src/page/MemberInfo/ListLayout/style.ts
@@ -1,12 +1,22 @@
 import { css } from "@emotion/react";
 
-export const attribute = css`
+export const attributeContainer = css`
   display: flex;
-  justify-content: flex-start;
+  justify-content: space-around;
   padding: 0 20px;
   margin: 20px 0;
   font-size: 20px;
   font-weight: bold;
-  gap: 50px;
+  gap: 20px;
   border-bottom: 1px solid #e0e0e0;
+`;
+
+export const attribute = css`
+  width: 70px;
+  text-align: center;
+`;
+
+export const longAttribute = css`
+  width: 120px;
+  text-align: center;
 `;
