002b2ac27f562b18ea5a60f7f6328e555a984b47
MinGu Jeong <mingu830@gmail.com>
2024-02-11 02:24:32 +0900

    feat: 리스트방식 회원정보 컨테이너

--- src/page/MemberInfo/ListLayout/index.tsx
+++ src/page/MemberInfo/ListLayout/index.tsx
@@ -1,5 +1,17 @@
+import * as S from './style';
+
 export default function ListLayout() {
   return(
-    <div>리스트 레이아웃</div>
+    <div css={S.attribute}>
+      <div>이름</div>
+      <div>트랙</div>
+      <div>직책</div>
+      <div>상태</div>
+      <div>소속</div>
+      <div>학부</div>
+      <div>학번</div>
+      <div>전화번호</div>
+      <div>이메일</div>
+      </div>
   )
 }
---
+++ src/page/MemberInfo/ListLayout/style.ts
@@ -0,0 +1,11 @@
+import { css } from "@emotion/react";
+
+export const attribute = css`
+  display: flex;
+  justify-content: flex-start;
+  padding: 0 20px;
+  margin: 20px 0;
+  font-size: 20px;
+  font-weight: bold;
+  gap: 50px;
+`;
