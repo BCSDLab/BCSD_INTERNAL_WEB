b86721e46e4d171d526239417e50f5b34ea5d89c
MinGu Jeong <mingu830@gmail.com>
2024-02-10 21:32:58 +0900

    refactor: 이미지 경로 변경

--- src/layout/DefaultLayout/index.tsx
+++ src/layout/DefaultLayout/index.tsx
@@ -5,7 +5,7 @@ export default function DefaultLayout() {
   return (
     <div css={S.container}>
       <div css={S.sideBar}>
-        <img src="/src/assets/image/BCSD_logo-01.png" alt="logo" css={S.logo} />
+        <img src="/public/BCSD_logo-01.png" alt="logo" css={S.logo} />
         <Button variant="outlined" color="secondary" sx={{ marginTop: '20px' }}>회원정보</Button>
       </div>
       <div css={S.content}>
