8bc509e3b23151046a846459485b3893a7d97853
MinGu Jeong <mingu830@gmail.com>
2024-02-11 00:20:00 +0900

    reafactor: 이미지 서버로 업로드하여 사용하도록 수정

--- src/layout/DefaultLayout/index.tsx
+++ src/layout/DefaultLayout/index.tsx
@@ -5,7 +5,7 @@ export default function DefaultLayout() {
   return (
     <div css={S.container}>
       <div css={S.sideBar}>
-        <img src="/BCSD_logo-01.png" alt="logo" css={S.logo} />
+        <img src="https://image.bcsdlab.com/banner.png" alt="logo" css={S.logo} />
         <Button variant="outlined" color="secondary" sx={{ marginTop: '20px' }}>회원정보</Button>
       </div>
       <div css={S.content}>
