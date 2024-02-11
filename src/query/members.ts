fc2490656469a2f6d46a15f166ae57fbf08e687f
MinGu Jeong <mingu830@gmail.com>
2024-02-11 15:21:20 +0900

    feat: members API 쿼리 작성

---
+++ src/query/members.ts
@@ -0,0 +1,8 @@
+import { useMutation } from "@tanstack/react-query";
+import { getMembers } from "api/members";
+
+export const useGetMembers = () => {
+  const {mutate} = useMutation({
+    
+  });
+};
