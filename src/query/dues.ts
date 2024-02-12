b7029b67e4108c3d4b8263d97c3e9fa5a2f43c91
dooohun <ehgns0305@naver.com>
2024-02-12 14:57:33 +0900

    refactor: data의 타입을 MemberShipDues로 수정

--- src/query/dues.ts
+++ src/query/dues.ts
@@ -1,8 +1,9 @@
 import { useSuspenseQuery } from "@tanstack/react-query"
 import { getAllDues } from "api/Dues"
+import { MemberShipDues } from "model/dues/allDues";
 
 export const useGetAllDues = (year: number, track?: string) => {
-  const { data: allDues } = useSuspenseQuery({
+  const { data: allDues }: {data: MemberShipDues} = useSuspenseQuery({
     queryKey: ["dues", year, track],
     queryFn: () => getAllDues(year, track),
   });
