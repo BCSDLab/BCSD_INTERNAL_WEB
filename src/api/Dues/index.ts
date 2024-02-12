79e9adfc0282c451c1e1af46e822d7168cf35ed2
dooohun <ehgns0305@naver.com>
2024-02-12 14:58:35 +0900

    refactor: 인터셉터를 추가함에 따라 fetcher를 바로 반환하도록 수정

--- src/api/Dues/index.ts
+++ src/api/Dues/index.ts
@@ -2,6 +2,5 @@ import { accessClient } from 'api';
 import { MemberShipDues } from 'model/dues/allDues';
 
 export const getAllDues = async (year: number, track?: string) => {
-  const response = await accessClient.get<MemberShipDues>(`/dues?year=${year}&track=${track}`);
-  return response;
+  return await accessClient.get<MemberShipDues>(`/dues?year=${year}&track=${track}`);
 };
