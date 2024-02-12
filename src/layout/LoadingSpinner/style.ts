f3fb4fafd6c8dfde0eba79ad88354392da5d926b
dooohun <ehgns0305@naver.com>
2024-02-12 16:07:30 +0900

    feat: LoadingSpinner 추가

---
+++ src/layout/LoadingSpinner/style.ts
@@ -0,0 +1,13 @@
+import { css } from "@emotion/react";
+
+export const loading = css`
+  position: fixed;
+  width: 100vw;
+  height: 100vh;
+  top: 0;
+  left: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(0, 0, 0, 0.1);
+`
