d206800644728888754419e4d2cc9c1275744669
chaeseungyun <thslr57@naver.com>
2024-02-13 23:42:16 +0900

    feat: 로그인 구현

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import DefaultLayout from 'layout/DefaultLayout';
 import MemberInfo from 'page/MemberInfo';
 import SignUp from 'page/SignUp';
+import SignIn from 'page/SignIn';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Route path="/" element={<DefaultLayout />} />
       <Route path="/" element={<MemberInfo />} />
       <Route path="/sign-up" element={<SignUp />} />
+      <Route path="/sign-in" element={<SignIn />} />
     </Routes>
   );
 }
