8769d60ab6abbe1e580e31ba02e772c3cbd3dfe5
MinGu Jeong <mingu830@gmail.com>
2024-02-17 02:53:01 +0900

    feat: 탈퇴회원 컴포넌트 분리

--- src/component/modal/memberInfoModal/style.ts
+++ src/component/modal/memberInfoModal/style.ts
@@ -4,3 +4,14 @@ export const textGap = css`
   display: flex;
   gap: 20px;
 `;
+
+export const buttonContainer = css`
+  display: flex;
+  justify-content: space-between;
+  gap: 30px;
+`;
+
+export const buttonWrapper = css`
+  display: flex;
+  gap: 20px;
+`;
