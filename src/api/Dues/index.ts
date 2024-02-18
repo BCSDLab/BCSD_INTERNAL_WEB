d5b79d0865ff3f0b175e100f5626cab93f7e0eaa
dooohun <ehgns0305@naver.com>
2024-02-19 00:52:06 +0900

    feat: putDues 추가

--- src/api/Dues/index.ts
+++ src/api/Dues/index.ts
@@ -6,7 +6,18 @@ export interface DuesOptions {
   track?: string;
 }
 
-export interface NewDuesProps {
+export interface PutDuesQuery {
+  memberId: number;
+  year: number;
+  month: number;
+}
+
+export interface PutDuesData {
+  status: 'PAID' | 'SKIP' | 'NOT_PAID' | null;
+  memo?: string | null;
+}
+
+export interface NewDuesData {
   memberId: number;
   year: number;
   month: number;
@@ -19,6 +30,10 @@ export const getAllDues = ({ year, track }: DuesOptions) => {
   return accessClient.get<DuesInfo>(query);
 };
 
-export const postDues = (data: NewDuesProps) => {
+export const postDues = (data: NewDuesData) => {
   return accessClient.post('/dues', data);
+};
+
+export const putDues = (data: NewDuesData) => {
+  return accessClient.put(`/dues?year=${data.year}&month=${data.month}&memberId=${data.memberId}`, { status: data.status, memo: data.memo });
 };
