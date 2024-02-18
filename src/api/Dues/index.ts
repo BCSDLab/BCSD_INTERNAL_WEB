018b7c9fe5e9c38d84b02e2f9a0f9b247612edd9
dooohun <ehgns0305@naver.com>
2024-02-18 19:26:02 +0900

    feat: POST /dues API 추가

--- src/api/Dues/index.ts
+++ src/api/Dues/index.ts
@@ -6,7 +6,19 @@ export interface DuesOptions {
   track?: string;
 }
 
+export interface NewDuesProps {
+  memberId: number;
+  year: number;
+  month: number;
+  status: 'PAID' | 'SKIP' | 'NOT_PAID' | null;
+  memo?: string | null;
+}
+
 export const getAllDues = ({ year, track }: DuesOptions) => {
   const query = track ? `/dues?year=${year}&track=${track}` : `/dues?year=${year}`;
   return accessClient.get<DuesInfo>(query);
+};
+
+export const postDues = (data: NewDuesProps) => {
+  return accessClient.post('/dues', data);
 };
