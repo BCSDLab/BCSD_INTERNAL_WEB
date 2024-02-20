5b663bf2a043896b786cd96423ca51f940762734
MinGu Jeong <mingu830@gmail.com>
2024-02-20 14:43:43 +0900

    feat: 마이페이지 기본레이아웃

---
+++ src/page/MyPage/index.tsx
@@ -0,0 +1,5 @@
+export default function MyPage() {
+  return (
+    <h1>MyPage</h1>
+  );
+}
---
+++ src/page/MyPage/style.ts
@@ -0,0 +1,13 @@
+import { css } from '@emotion/react';
+import { colors } from 'const/colors/style';
+
+export const topBarTitle = css`
+  margin-left: 20px;
+`;
+
+export const container = css`
+  display: flex;
+  background-color: ${colors.gray};
+  width: 100%;
+  height: 100vh;
+`;
