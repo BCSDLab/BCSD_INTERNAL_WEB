9c4540e870d834b4cd0d85404666cb1dcc6732f3
dooohun <ehgns0305@naver.com>
2024-02-22 04:44:09 +0900

    fix: merge conflict 수정

--- src/page/DuesSetup/style.ts
+++ src/page/DuesSetup/style.ts
@@ -17,10 +17,6 @@ export const sidebar = css`
   border-right: 1px solid ${colors.borderGray};
 `;
 
-export const tableCell = css`
-  width: 100px;
-`;
-
 export const logo = css`
   width: 100%;
   margin-top: -10px;
