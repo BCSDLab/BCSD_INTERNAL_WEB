05db0bd332881458afe9ca0172335161ef6aac0c
MinGu Jeong <mingu830@gmail.com>
2024-02-28 03:45:24 +0900

    feat: grid layout 회원정보 조회

--- src/page/MemberInfo/GridLayout/index.tsx
+++ src/page/MemberInfo/GridLayout/index.tsx
@@ -2,19 +2,80 @@ import { useGetMembers, useGetMembersDeleted } from 'query/members';
 import { useTrackStore } from 'store/trackStore';
 import { Member, STATUS_LABEL } from 'model/member';
 import Grid from '@mui/material/Grid';
+import { useState } from 'react';
+import MemberInfoModal from 'component/modal/memberInfoModal';
+import { Paper, styled } from '@mui/material';
 import * as S from './style';
 
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+  ':hover': {
+    cursor: 'pointer',
+    boxShadow: theme.shadows[4],
+  },
+}));
+
+const STATUS_LIST = ['ATTEND', 'OFF', 'IPP', 'ARMY', 'COMPLETION', 'GRADUATE'] as const;
+
 export default function GridLayout() {
+  const defaultImageUrl = 'https://image.bcsdlab.com/default-profile.png';
   const { id } = useTrackStore();
   const { data: members } = useGetMembers({ pageIndex: 0, pageSize: 1000, trackId: id });
+  const [modalOpen, setModalOpen] = useState(false);
+  const [memberInfo, setMemberInfo] = useState<Member | null>(null);
   const { data: membersDeleted } = useGetMembersDeleted({
     pageIndex: 0, pageSize: 1000, trackId: id,
   });
+  const handleOpenModal = () => {
+    setModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setModalOpen(false);
+  };
+
   return (
     <div css={S.container}>
-      <div css={S.memberTypeContainer} />
-      <div css={S.memberTypeContainer}>REGULAR</div>
-      <div css={S.memberTypeContainer}>BEGINNER</div>
+      <div css={S.memberContainer}>
+        <Grid container spacing={3}>
+          {members?.content.map((member: Member) => (
+            <Grid item xs={3} key={member.id}>
+              <Item
+                css={S.memberContainer}
+                onClick={() => {
+                  setMemberInfo(member);
+                  handleOpenModal();
+                }}
+              >
+                <div css={S.memberWrapper}>
+                  <div css={S.imageNameWrapper}>
+                    <img css={S.image} src={member.profileImageUrl || defaultImageUrl} alt="profile" />
+                    <div>{member.name}</div>
+                  </div>
+                  <div>
+                    {STATUS_LABEL[member.status]}
+                  </div>
+                  <div>{member.memberType}</div>
+                  <div>{member.studentNumber}</div>
+                  <div>{member.company}</div>
+                  <div>{member.department}</div>
+                  <div>{member.phoneNumber}</div>
+                  <div>{member.email}</div>
+                </div>
+              </Item>
+            </Grid>
+          ))}
+        </Grid>
+      </div>
+      <MemberInfoModal
+        open={modalOpen}
+        onClose={handleCloseModal}
+        member={memberInfo}
+      />
     </div>
 
   );
--- src/page/MemberInfo/GridLayout/style.ts
+++ src/page/MemberInfo/GridLayout/style.ts
@@ -6,9 +6,35 @@ export const container = css`
   gap: 20px;
 `;
 
-export const memberTypeContainer = css`
+export const memberContainer = css`
   display: flex;
   justify-content: center;
   align-items: center;
   gap: 10px;
+  margin: 10px 10px 0 10px;
+`;
+
+export const memberWrapper = css`
+  width: 300px;
+  height: 380px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  padding-top: 20px;
+  gap: 15px;
+`;
+
+export const imageNameWrapper = css`
+  width: 100%;
+  height: 100px;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+`;
+
+export const image = css`
+  width: 50%;
+  height: 100%;
+  object-fit: scale-down;
 `;
