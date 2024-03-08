e4439fb463ea71dd1283204bbdbbda13efcf58e8
MinGu Jeong <mingu830@gmail.com>
2024-03-09 02:24:57 +0900

    feat: sidebar style 적용

--- src/layout/SideBar/index.tsx
+++ src/layout/SideBar/index.tsx
@@ -13,15 +13,14 @@ export default function SideBar() {
     <div css={S.container}>
       <div css={S.sideBar}>
         <img src="https://image.bcsdlab.com/banner.png" alt="logo" css={S.logo} />
-        <Button color="secondary" sx={{ marginTop: '20px' }} size="large" onClick={() => navigate('member')}>회원정보</Button>
-        <Button color="secondary" sx={{ marginTop: '20px' }} size="large" onClick={() => navigate('mypage')}>마이페이지</Button>
-        <Button color="secondary" sx={{ marginTop: '20px' }} size="large" onClick={() => navigate('accept')}>회원승인</Button>
-        <Button color="secondary" sx={{ marginTop: '20px' }} size="large" onClick={() => navigate('dues')}>회비납부</Button>
-        <Button color="secondary" sx={{ marginTop: '20px' }} size="large" onClick={() => navigate('dues-setup')}>회비생성</Button>
-        <Button color="secondary" sx={{ marginTop: '20px' }} size="large" onClick={() => navigate('edit-dues')}>회비수정</Button>
-        <Button color="secondary" sx={{ marginTop: '20px' }} size="large" onClick={() => navigate('track')}>트랙정보</Button>
+        <Button color="secondary" sx={{ marginTop: '20px' }} size="large" onClick={() => navigate('member')} css={S.button('20px')}>회원정보</Button>
+        <Button color="secondary" sx={{ marginTop: '20px' }} size="large" onClick={() => navigate('mypage')} css={S.button('60px')}>마이페이지</Button>
+        <Button color="secondary" sx={{ marginTop: '20px' }} size="large" onClick={() => navigate('accept')} css={S.button('100px')}>회원승인</Button>
+        <Button color="secondary" sx={{ marginTop: '20px' }} size="large" onClick={() => navigate('dues')} css={S.button('140px')}>회비납부</Button>
+        <Button color="secondary" sx={{ marginTop: '20px' }} size="large" onClick={() => navigate('dues-setup')} css={S.button('180px')}>회비생성</Button>
+        <Button color="secondary" sx={{ marginTop: '20px' }} size="large" onClick={() => navigate('edit-dues')} css={S.button('220px')}>회비수정</Button>
         <div css={S.logOutButton}>
-          <Button variant="contained" color="secondary" sx={{ marginTop: '30vh', width: '100px' }} onClick={logOut}>로그아웃</Button>
+          <Button variant="contained" color="secondary" sx={{ marginTop: '20vh', width: '100px' }} onClick={logOut}>로그아웃</Button>
         </div>
       </div>
     </div>
