620f5430c01484a192e27cf4a1dec84401068ae0
dooohun <ehgns0305@naver.com>
2024-03-25 00:22:02 +0900

    feat: 동방 예약 API 추가

--- src/model/reservations.ts
+++ src/model/reservations.ts
@@ -1,15 +1,12 @@
-type Year = `${number}`;
-type Month = `${number}`;
-type Day = `${number}`;
-type Hour = `${number}`;
-type Minute = `${number}`;
-
-export type DateTimeFormat = `${Year}-${Month}-${Day} ${Hour}:${Minute}`;
-
 export interface Reservations {
   memberCount: number;
   reason: string;
   detailedReason: string;
-  startDateTime: DateTimeFormat;
-  endDateTime: DateTimeFormat;
+  startDateTime: string;
+  endDateTime: string;
+}
+
+export interface GetReservationsResponse extends Reservations {
+  id: number;
+  memberName: string;
 }
