d83becc87db56dc5620ed845e6f860907ee9215e
chaeseungyun <thslr57@naver.com>
2024-03-25 15:15:14 +0900

    refactor: 예약 관련 api 생성

--- src/api/reservations/index.ts
+++ src/api/reservations/index.ts
@@ -1,6 +1,6 @@
 import { accessClient } from 'api';
 import { Reservations } from 'model/reservations';
 
-export const getReservations = () => accessClient.get('/reservations');
+export const getReservations = () => accessClient.get<Reservations[]>('/reservations');
 
 export const postReservations = (data: Reservations) => accessClient.post('/reservations', data);
