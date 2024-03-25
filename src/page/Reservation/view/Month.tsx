4fff630357558277ea2d8a0a1eb57cf807f25131
chaeseungyun <thslr57@naver.com>
2024-03-25 15:14:48 +0900

    feat: 캘린더 UI 완성

--- src/page/Reservation/view/Month.tsx
+++ src/page/Reservation/view/Month.tsx
@@ -1,46 +1,74 @@
 import {
-  Table, TableBody, TableCell, TableContainer, TableHead, TableRow,
+  Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Button,
 } from '@mui/material';
-// import * as S from './style';
+import { ArrowBackIosNewOutlined, ArrowForwardIosOutlined } from '@mui/icons-material';
+import { useGetReservations } from 'query/reservations';
+import { Reservations } from 'model/reservations';
+import { useEffect, useState } from 'react';
+import * as S from './style';
+// eslint-disable-next-line import/no-cycle
+import MonthModal from './Month/MonthModal';
 
 type Calendar = {
   day: number | null,
   date: number | null,
-}[];
+  today: string | null,
+};
 
-const date = new Date();
-const currentMonth = date.getMonth();
-const currentYear = date.getFullYear();
+type Calendars = Calendar[];
+
+export type CalendarContent = {
+  data: Reservations[],
+  date: number | null,
+  today: string | null,
+  currentMonth: number,
+};
+
+function CalendarCell({
+  date, today, data, currentMonth,
+}: CalendarContent) {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  const filteredData = data.filter((item) => item.startDateTime.slice(0, 10) === today);
+
+  return (
+    <>
+      <TableCell css={S.Cell(currentMonth === new Date().getMonth() && date === new Date().getDate())} onClick={() => date && handleOpen()}>
+        <div css={S.Date(currentMonth === new Date().getMonth() && date === new Date().getDate())}>
+          {date}
+        </div>
+        <div>
+          {date && filteredData.map((item, index) => (
+            <p css={S.schedule(index)}>
+              {item.startDateTime.slice(10, 20)}
+              {' '}
+              {item.reason}
+            </p>
+          ))}
+        </div>
+      </TableCell>
+      <MonthModal today={today} date={date} data={filteredData} handleClose={handleClose} open={open} currentMonth={currentMonth} />
+    </>
+  );
+}
 
 // 각 요일 구하기
-const getCurrentDay = (day: number) => {
+const getCurrentDay = (currentYear: number, currentMonth: number, day: number) => {
   return new Date(currentYear, currentMonth, day).getDay();
 };
 
-// 현재 월의 일 수
-const currentMonthDate = new Date(currentYear, currentMonth + 1, 0).getDate();
-const calendar = Array.from({ length: currentMonthDate }, (_, i) => {
-  const obj = {
-    day: getCurrentDay(i),
-    date: i + 1,
-  };
-
-  return obj;
-});
-
-const emptySpace = (calendars: Calendar) => {
+const emptySpace = (calendars: Calendars) => {
   if (calendars[0].day) {
     const length = calendars[0].day;
     for (let i = 0; i < length; i += 1) {
-      calendars.unshift({ day: null, date: null });
+      calendars.unshift({ day: null, date: null, today: null });
     }
   }
 };
-emptySpace(calendar);
-console.log(calendar);
 
-const chunkArray = (arr: Calendar, chunkSize: number) => {
-  const result: Calendar[] = [];
+const chunkArray = (arr: Calendars, chunkSize: number) => {
+  const result: Calendars[] = [];
   for (let i = 0; i < arr.length; i += chunkSize) {
     const chunk = arr.slice(i, i + chunkSize);
     result.push(chunk);
@@ -48,11 +76,56 @@ const chunkArray = (arr: Calendar, chunkSize: number) => {
   return result;
 };
 
-const weeklyCalendar = chunkArray(calendar, 7);
-
 export default function Month() {
+  const { data } = useGetReservations();
+  const [currentYear, setCurrentYear] = useState(new Date().getFullYear());
+  const [currentMonth, setCurrentMonth] = useState(new Date().getMonth());
+  const [currentCalendar, setCurrentCalendar] = useState<Calendars[]>([]);
+
+  const nextYear = () => setCurrentYear((prev) => prev + 1);
+  const nextMonth = () => setCurrentMonth((prev) => prev + 1);
+  const previousYear = () => setCurrentYear((prev) => prev - 1);
+  const previousMonth = () => setCurrentMonth((prev) => prev - 1);
+
+  useEffect(() => {
+    // 현재 월의 일 수
+    const currentMonthDate = new Date(currentYear, currentMonth + 1, 0).getDate();
+
+    const calendar = Array.from({ length: currentMonthDate }, (_, i) => {
+      const index = i + 1;
+      const obj: Calendar = {
+        day: getCurrentDay(currentYear, currentMonth, index),
+        date: index,
+        today: new Date(currentYear, currentMonth, index + 1).toISOString().slice(0, 10),
+      };
+
+      return obj;
+    });
+
+    emptySpace(calendar);
+    const weeklyCalendar = chunkArray(calendar, 7);
+
+    setCurrentCalendar(weeklyCalendar);
+  }, [currentMonth, currentYear]);
+
   return (
     <TableContainer>
+      <div css={S.AlignItems}>
+        <Button onClick={previousYear}>
+          <ArrowBackIosNewOutlined />
+        </Button>
+        <div css={S.current}>{`${currentYear}년`}</div>
+        <Button onClick={nextYear}>
+          <ArrowForwardIosOutlined />
+        </Button>
+        <Button onClick={previousMonth}>
+          <ArrowBackIosNewOutlined />
+        </Button>
+        <div css={S.current}>{`${currentMonth + 1}월`}</div>
+        <Button onClick={nextMonth}>
+          <ArrowForwardIosOutlined />
+        </Button>
+      </div>
       <Table>
         <TableHead>
           <TableRow>
@@ -66,10 +139,10 @@ export default function Month() {
           </TableRow>
         </TableHead>
         <TableBody>
-          {weeklyCalendar.map((week, weekIndex) => (
-            <TableRow key={week[weekIndex].date}>
+          {currentCalendar.map((week) => (
+            <TableRow>
               {week.map((day) => (
-                <TableCell key={day.day}>{day.date}</TableCell>
+                <CalendarCell today={day.today} date={day.date} data={data} currentMonth={currentMonth} />
               ))}
             </TableRow>
           ))}
--- src/page/Reservation/view/style.ts
+++ src/page/Reservation/view/style.ts
@@ -1,6 +1,53 @@
 import { css } from '@emotion/react';
+import { colors } from 'const/colors/style';
+
+const background = [
+  '#FCB9AA', '#FFDBCC', '#C6DBDA', 'A2E1DB', '55CBCD',
+];
 
 export const layout = css`
   display: flex;
   flex-wrap: wrap;
+`;
+
+export const Cell = (isToday: boolean) => css`
+  border: 1px solid ${colors.borderGray};
+  box-sizing: border-box;
+  height: 100px;
+  width: calc(100% / 7);
+  position: relative;
+  overflow: hidden;
+  background-color: ${isToday && '#ECEAE4'};
+`;
+
+export const Date = (isToday: boolean) => css`
+  position: absolute;
+  top: 2px;
+  left: 2px;
+  font-weight: ${isToday && 'bold'};
+`;
+
+export const schedule = (index: number) => css`
+  width: 150px;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  overflow: hidden;
+  background-color: ${background[index]};
+  border-radius: 10px;
+  padding-left: 10px;
+  margin-bottom: 3px;
+`;
+
+export const space = css`
+  height: 20px;
+`;
+
+export const AlignItems = css`
+  display: flex;
+  align-items: center;
+`;
+
+export const current = css`
+  width: 100px;
+  text-align: center;
 `;
