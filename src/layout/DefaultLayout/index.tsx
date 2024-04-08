550856f55ebad3fe098b22bc7354a67f95535719
chaeseungyun <thslr57@naver.com>
2024-04-08 13:15:19 +0900

    refactor: 사이드바를 drawer 형태로 보여주도록 수정

--- src/layout/DefaultLayout/index.tsx
+++ src/layout/DefaultLayout/index.tsx
@@ -4,12 +4,14 @@ import SideBar from 'layout/SideBar';
 import { useLoginState } from 'store/loginStore';
 import { useSnackBar } from 'ts/useSnackBar';
 import { PATHS } from 'util/constants/path';
+import useBooleanState from 'util/hooks/useBooleanState';
 import * as S from './style';
 
 const beginnerPage = [PATHS.track, PATHS.role, PATHS.myPage];
 
 export default function DefaultLayout() {
   const { me } = useLoginState();
+  const { value: open, setFalse: onClose, setTrue: openSideBar } = useBooleanState(false);
   const navigate = useNavigate();
   const location = useLocation();
   const openSnackBar = useSnackBar();
@@ -19,15 +21,16 @@ export default function DefaultLayout() {
     openSnackBar({ type: 'error', message: '접근 권한이 없습니다.' });
     navigate('/track');
   }
+
   return (
     <div css={S.Layout}>
-      <SideBar />
       <div css={S.width}>
-        <TopBar />
+        <TopBar openSideBar={openSideBar} onClose={onClose} />
         <div css={S.content}>
           <Outlet />
         </div>
       </div>
+      <SideBar open={open} onClose={onClose} />
     </div>
   );
 }
--- src/layout/SideBar/index.tsx
+++ src/layout/SideBar/index.tsx
@@ -1,108 +1,106 @@
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
-import { Button } from '@mui/material';
+import { Button, Drawer } from '@mui/material';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { useState } from 'react';
 import { KeyboardDoubleArrowDown, KeyboardDoubleArrowUp } from '@mui/icons-material';
 import { pagePath } from 'layout/TopBar';
-import { useLoginState } from 'store/loginStore';
 import { PATHS } from 'util/constants/path';
 import * as S from './style';
 
-export default function SideBar() {
+interface Props {
+  open: boolean;
+  onClose: () => void;
+}
+
+export default function SideBar({ open, onClose }: Props) {
   const navigate = useNavigate();
-  const { deleteMe } = useLoginState();
-  const logOut = () => {
-    deleteMe();
-    navigate(PATHS.home);
-  };
   const [isClicked, setIsClicked] = useState(false);
   const location = useLocation();
   const currentPage = pagePath.filter((path) => location.pathname === path.path)[0].path;
 
   return (
-    <div css={S.container}>
-      <div css={S.sideBar}>
-        <img src="https://image.bcsdlab.com/banner.png" alt="logo" css={S.logo} onClick={() => navigate(PATHS.home)} />
-        <Button
-          color="secondary"
-          sx={{ marginTop: '20px' }}
-          size="large"
-          onClick={() => navigate('member')}
-          css={S.button(currentPage === PATHS.member)}
-        >
-          회원 정보
-        </Button>
-        <Button
-          color="secondary"
-          sx={{ marginTop: '20px' }}
-          size="large"
-          onClick={() => navigate('mypage')}
-          css={S.button(currentPage === PATHS.myPage)}
-        >
-          마이페이지
-        </Button>
-        <Button
-          color="secondary"
-          sx={{ marginTop: '20px' }}
-          size="large"
-          onClick={() => navigate('accept')}
-          css={S.button(currentPage === PATHS.accept)}
-        >
-          회원 승인
-        </Button>
-        <Button color="secondary" sx={{ marginTop: '20px' }} size="large" onClick={() => navigate('track')} css={S.button(currentPage === PATHS.track)}>트랙 정보</Button>
-        <Button color="secondary" sx={{ marginTop: '20px' }} size="large" onClick={() => navigate('team')} css={S.button(currentPage === PATHS.team)}>팀 정보</Button>
-        <Button color="secondary" sx={{ marginTop: '20px' }} size="large" onClick={() => navigate('role')} css={S.button(currentPage === PATHS.role)}>직책 정보</Button>
-        <Button color="secondary" sx={{ marginTop: '20px' }} size="large" onClick={() => navigate('reservation')} css={S.button(currentPage === PATHS.reservation)}>예약 정보</Button>
+    <Drawer open={open} onClose={onClose}>
+      <div css={S.container}>
+        <div css={S.sideBar}>
+          <img src="https://image.bcsdlab.com/banner.png" alt="logo" css={S.logo} onClick={() => navigate(PATHS.home)} />
+          <Button
+            color="secondary"
+            sx={{ marginTop: '20px' }}
+            size="large"
+            onClick={() => navigate('member')}
+            css={S.button(currentPage === PATHS.member)}
+          >
+            회원 정보
+          </Button>
+          <Button
+            color="secondary"
+            sx={{ marginTop: '20px' }}
+            size="large"
+            onClick={() => navigate('mypage')}
+            css={S.button(currentPage === PATHS.myPage)}
+          >
+            마이페이지
+          </Button>
+          <Button
+            color="secondary"
+            sx={{ marginTop: '20px' }}
+            size="large"
+            onClick={() => navigate('accept')}
+            css={S.button(currentPage === PATHS.accept)}
+          >
+            회원 승인
+          </Button>
+          <Button color="secondary" sx={{ marginTop: '20px' }} size="large" onClick={() => navigate('track')} css={S.button(currentPage === PATHS.track)}>트랙 정보</Button>
+          <Button color="secondary" sx={{ marginTop: '20px' }} size="large" onClick={() => navigate('team')} css={S.button(currentPage === PATHS.team)}>팀 정보</Button>
+          <Button color="secondary" sx={{ marginTop: '20px' }} size="large" onClick={() => navigate('role')} css={S.button(currentPage === PATHS.role)}>직책 정보</Button>
+          <Button color="secondary" sx={{ marginTop: '20px' }} size="large" onClick={() => navigate('reservation')} css={S.button(currentPage === PATHS.reservation)}>예약 정보</Button>
 
-        <Button
-          color="secondary"
-          sx={{ marginTop: '20px' }}
-          size="large"
-          onClick={() => setIsClicked((prev) => !prev)}
-          css={S.duesButton}
-        >
-          회비
-          {isClicked ? <KeyboardDoubleArrowUp /> : <KeyboardDoubleArrowDown />}
-        </Button>
-        {
-          isClicked && (
-            <div css={S.duesGroup}>
-              <Button
-                color="secondary"
-                sx={{ marginTop: '20px' }}
-                size="large"
-                onClick={() => navigate('dues')}
-                css={S.button(currentPage === PATHS.dues)}
-              >
-                납부 내역
-              </Button>
-              <Button
-                color="secondary"
-                sx={{ marginTop: '20px' }}
-                size="large"
-                onClick={() => navigate('dues-setup')}
-                css={S.button(currentPage === PATHS.duesSetup)}
-              >
-                회비 생성
-              </Button>
-              <Button
-                color="secondary"
-                sx={{ marginTop: '20px' }}
-                size="large"
-                onClick={() => navigate('edit-dues')}
-                css={S.button(currentPage === PATHS.editDues)}
-              >
-                회비 수정
-              </Button>
-            </div>
-          )
-        }
-        <div css={S.logOutButton}>
-          <Button variant="contained" color="secondary" sx={{ width: '100px' }} onClick={logOut}>로그아웃</Button>
+          <Button
+            color="secondary"
+            sx={{ marginTop: '20px' }}
+            size="large"
+            onClick={() => setIsClicked((prev) => !prev)}
+            css={S.duesButton}
+          >
+            회비
+            {isClicked ? <KeyboardDoubleArrowUp /> : <KeyboardDoubleArrowDown />}
+          </Button>
+          {
+            isClicked && (
+              <div css={S.duesGroup}>
+                <Button
+                  color="secondary"
+                  sx={{ marginTop: '20px' }}
+                  size="large"
+                  onClick={() => navigate('dues')}
+                  css={S.button(currentPage === PATHS.dues)}
+                >
+                  납부 내역
+                </Button>
+                <Button
+                  color="secondary"
+                  sx={{ marginTop: '20px' }}
+                  size="large"
+                  onClick={() => navigate('dues-setup')}
+                  css={S.button(currentPage === PATHS.duesSetup)}
+                >
+                  회비 생성
+                </Button>
+                <Button
+                  color="secondary"
+                  sx={{ marginTop: '20px' }}
+                  size="large"
+                  onClick={() => navigate('edit-dues')}
+                  css={S.button(currentPage === PATHS.editDues)}
+                >
+                  회비 수정
+                </Button>
+              </div>
+            )
+          }
         </div>
       </div>
-    </div>
+    </Drawer>
   );
 }
