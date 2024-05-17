aafc012c07d137c8489469080fcf8d9a6339e749
dooohun <ehgns0305@naver.com>
2024-05-17 20:06:41 +0900

    refactor: useQueryParam 타입을 string으로 고정

--- src/util/hooks/useQueryParam.ts
+++ src/util/hooks/useQueryParam.ts
@@ -1,24 +1,10 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 
-export default function useQueryParam(key: string, type: string): string | number | null {
+export default function useQueryParam(name: string) {
   const location = useLocation();
-  const [param, setParam] = useState<string | number | null>(null);
+  const searchParams = new URLSearchParams(location.search);
+  const value = searchParams.get(name) as string | undefined;
 
-  useEffect(() => {
-    const searchParams = new URLSearchParams(location.search);
-    const paramValue = searchParams.get(key);
-    if (paramValue) {
-      if (type === 'number') {
-        setParam(parseInt(paramValue, 10));
-      } else {
-        setParam(paramValue);
-      }
-    } else {
-      setParam(null);
-    }
-  }, [location.search, key, type]);
-
-  return param;
+  return value;
 }
