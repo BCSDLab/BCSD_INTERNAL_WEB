3485d88bf05dea4ec57993b2266dfe86fabeb783
dooohun <ehgns0305@naver.com>
2025-01-06 23:04:58 +0900

    fix: 페이지네이션 navigate 시 pageName에 속한 페이지로 이동하도록 변경

--- src/component/YearPagination/index.tsx
+++ src/component/YearPagination/index.tsx
@@ -1,34 +1,42 @@
-import { ArrowBackIosNewOutlined, ArrowForwardIosOutlined } from '@mui/icons-material';
-import { Button } from '@mui/material';
-import { useNavigate } from 'react-router-dom';
-import { LAST_DUES_YEAR } from 'util/constants/status';
-import { useQueryParam } from 'util/hooks/useQueryParam';
-import * as S from './style';
+import {
+  ArrowBackIosNewOutlined,
+  ArrowForwardIosOutlined,
+} from "@mui/icons-material";
+import { Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import { LAST_DUES_YEAR } from "util/constants/status";
+import { useQueryParam } from "util/hooks/useQueryParam";
+import * as S from "./style";
 
 interface YearPaginationProps {
+  pageName: string;
   duesYear: number;
   setDuesYear: React.Dispatch<React.SetStateAction<number>>;
   routeParam: string;
 }
 
-export default function YearPagination({ duesYear, setDuesYear, routeParam }: YearPaginationProps) {
+export default function YearPagination({
+  pageName,
+  duesYear,
+  setDuesYear,
+}: YearPaginationProps) {
   const navigate = useNavigate();
   const currentYear = new Date().getFullYear();
-  const param = useQueryParam('page');
+  const param = useQueryParam("page");
   const page = Number(param);
 
   const goToPrevYear = () => {
     // 재학생 회비 내역이 2021년부터 시작하므로 2021년 이전으로 이동할 수 없음
     const prevYear = page ? page + 1 : 2;
     if (prevYear <= currentYear - 2020) {
-      navigate(`/${routeParam}?page=${prevYear}`);
+      navigate(`/dues?page=${prevYear}`);
       setDuesYear((prev) => prev - 1);
     }
   };
 
   const goToNextYear = () => {
     if (page && page > 1) {
-      navigate(`/${routeParam}?page=${page - 1}`);
+      navigate(`/${pageName}?page=${page - 1}`);
       setDuesYear((prev) => prev + 1);
     }
   };
